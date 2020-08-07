94269edc7d47c9ae208ca0c900e8e3172f4f7a2f
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 00:23:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,8 +1,8 @@
 # Add your code here
 # class variable @@all set to an empty array inside your class
 # .new initializes with one argument - a name
-#  class variables @@all
-      has a class variable, @@all, the points to an array  
+#  class variables @@allhas a class variable, @@all, the points to an array
+        
    
 # 
 # 
