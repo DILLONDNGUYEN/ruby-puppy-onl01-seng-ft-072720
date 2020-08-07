7733075aa4e5757e6ffc87897fed7d29fc2715aa
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 00:24:51 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -3,7 +3,8 @@
 # .new initializes with one argument - a name
 #  class variables @@all has a class variable, @@all, the points to an array
 # class methods .all is a class method returns all dog instances
-#      
+#   .clear_all
+      is a class method that empties the @@all array of all existing dogs   
     
 # 
 
