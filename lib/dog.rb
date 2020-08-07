2a889b64f3ab10186a9259979af13e709408b9ba
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 00:25:58 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -5,7 +5,8 @@
 # class methods .all is a class method returns all dog instances
 # .clear_all is a class method that empties the @@all array of all existing dogs 
 # .print_all is a class method that puts out the name of each dog to the terminal 
-#    
+#  #save
+    adds this dog instance to the @@all array when called  
 # 
 
 
