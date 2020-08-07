dbd53a3d2e4de0b6836f2e8cf5a8f529f7a6789d
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 00:24:26 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -2,8 +2,8 @@
 # class variable @@all set to an empty array inside your class
 # .new initializes with one argument - a name
 #  class variables @@all has a class variable, @@all, the points to an array
-# class methods.all
-      is a class method returns all dog instances
+# class methods .allis a class method returns all dog instances
+      
     
 # 
 
