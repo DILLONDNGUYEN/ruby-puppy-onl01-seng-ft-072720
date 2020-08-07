6c6940b087ca58717fddb6cfd79d98f3382509b4
Dillon Nguyen <dillon.d.nguyen@gmail.com>
2020-08-07 00:25:38 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,11 +1,10 @@
 # Add your code here
 # class variable @@all set to an empty array inside your class
 # .new initializes with one argument - a name
-#  class variables @@all has a class variable, @@all, the points to an array
+# class variables @@all has a class variable, @@all, the points to an array
 # class methods .all is a class method returns all dog instances
 # .clear_all is a class method that empties the @@all array of all existing dogs 
-#  .print_allis a class method that puts out the name of each dog to the terminal 
-           
+# .print_all is a class method that puts out the name of each dog to the terminal 
 #    
 # 
 
